cd7f9af9902fcbee722cad923388f63ae20a5db1
meicich <macalonabc30@gmail.com>
2023-11-14 15:54:34 +0900

    Automated commit

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import schedule
-import time
+
 import datetime
 import os
 
-def job():
+for i in range(0,10):
     filename = datetime.datetime.now().strftime("./%Y-%m-%d.txt")
     with open(filename, 'w') as f:
         f.write(datetime.datetime.now().strftime("%Y-%m-%d"))
     os.system('git add *')
     os.system('git commit -m "Automated commit"')
     os.system('git push')
-
-schedule.every().day.at("00:00").do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-
